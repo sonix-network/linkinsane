ca22295abf1b241c1d3e16e4806c31fd88339053
Christian Svensson <blue@cmd.nu>
2024-08-28 13:59:17 +0200

    More logging and IPv6 support

--- receiver.py
+++ receiver.py
@@ -5,6 +5,18 @@ import logging
 import signal
 import zmq
 import sys
+from zmq.utils.monitor import recv_monitor_message
+
+
+def event_monitor(logger, monitor: zmq.Socket, connected) -> None:
+    while monitor.poll():
+        evt = recv_monitor_message(monitor)
+        if evt['event'] == zmq.EVENT_HANDSHAKE_SUCCEEDED:
+            logger.info('Peer handshake succeeded')
+            connected.set()
+        if evt['event'] == zmq.EVENT_MONITOR_STOPPED:
+            break
+    monitor.close()
 
 
 def action(logger, x, s):
@@ -19,10 +31,16 @@ def main():
     logger.info('linkinsane receiver starting up on interface %s, listening on port 5555', iface)
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
+    socket.setsockopt(zmq.IPV6, True)
     socket.bind('tcp://*:5555')
 
+    monitor = socket.get_monitor_socket()
+    connected = threading.Event()
+    mt = threading.Thread(target=event_monitor, args=(logger, monitor, connected))
+    mt.start()
+
     conf.use_pcap = True
-    t = AsyncSniffer(iface=iface, promisc=True, prn=lambda x: action(logger, x, socket), store=False)
+    t = AsyncSniffer(iface=iface, promisc=True, prn=lambda x: action(logger, x, socket), store=False, filter='not ip host 10.10.10.1')
     t.start()
 
     try:
@@ -30,6 +48,7 @@ def main():
     except KeyboardInterrupt:
         print('Ok, shutting down')
 
+    socket.close()
     t.stop()
     t.join()
 
